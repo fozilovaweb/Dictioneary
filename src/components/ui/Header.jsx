6c4c42a691eb4ca557d42375c2feaf2ceab29031
fozilovaweb <zaynabxon23@gmail.com>
2025-11-28 19:59:47 +0500

    Secondry style

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { RiBook2Line } from "react-icons/ri";
+
+function Header() {
+  return (
+    <div>
+      <RiBook2Line />
+    </div>
+  );
+}
+
+export default Header;
